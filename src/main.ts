2c84e91f45257f38e977717b0bd224ed57f74b15
Ronny Ancorini Rodríguez <ronnyancorini@gmail.com>
2022-11-18 21:35:21 +0100

    chore(main): 🔧 test prettier pre commit hook

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@
  * Some predefined delay values (in milliseconds).
  */
 export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000
+    Short = 500,
+    Medium = 2000,
+    Long = 5000
 }
 
 /**
@@ -15,12 +15,12 @@ export enum Delays {
  * @returns {Promise<string>}
  */
 function delayedHello(
-  name: string,
-  delay: number = Delays.Medium
+    name: string,
+    delay: number = Delays.Medium
 ): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay)
-  )
+    return new Promise((resolve: (value?: string) => void) =>
+        setTimeout(() => resolve(`Hello, ${name}`), delay)
+    )
 }
 
 // Please see the comment in the .eslintrc.json file about the suppressed rule!
@@ -29,7 +29,7 @@ function delayedHello(
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export async function greeter(name: string) {
-  // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long)
+    // eslint-disable-line @typescript-eslint/no-explicit-any
+    // The name parameter should be of type string. Any is used only to trigger the rule.
+    return await delayedHello(name, Delays.Long)
 }
